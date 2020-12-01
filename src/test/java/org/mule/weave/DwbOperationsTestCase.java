6db54847667881c0444d1ca6cb6f8af94b769636
msivori <msivori@mulesoft.com>
2020-12-01 19:51:15 -0300

    add Test

--- src/test/java/org/mule/weave/DwbOperationsTestCase.java
+++ src/test/java/org/mule/weave/DwbOperationsTestCase.java
@@ -4,10 +4,24 @@ import org.junit.Test;
 import org.mule.functional.api.flow.FlowRunner;
 import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import org.mule.runtime.weave.dwb.api.IWeaveValue;
+import org.mule.runtime.weave.dwb.api.WeaveDOMReader;
+import org.mule.runtime.weave.dwb.api.WeaveStreamFactoryService;
+import org.mule.runtime.weave.dwb.api.WeaveStreamWriter;
+import org.mule.weave.utils.PrinterVisitor;
+
+import javax.inject.Inject;
+
 public class DwbOperationsTestCase extends MuleArtifactFunctionalTestCase {
+
+  @Inject
+  private WeaveStreamFactoryService dwbService;
 
   /**
    * Specifies the mule config xml with the flows that are going to be executed in the tests, this file lives in the test resources.
@@ -58,4 +72,34 @@ public class DwbOperationsTestCase extends MuleArtifactFunctionalTestCase {
     assertThat(payloadValue, is(expectedPayload));
   }
 
+  @Test
+  public void testPrintWrite() throws Exception{
+    ByteArrayOutputStream bot = new ByteArrayOutputStream();
+    ByteArrayOutputStream bot2 = new ByteArrayOutputStream();
+
+    WeaveStreamWriter wsw = dwbService.getFactory().createStreamWriter(bot);
+    wsw.writeStartDocument()
+            .writeStartObject()
+            .writeKey("message")
+            .writeString("B2B Rul3z")
+            .writeEndObject();
+
+    WeaveStreamWriter wsw2 = dwbService.getFactory().createStreamWriter(bot2);
+    wsw2.writeStartDocument()
+            .writeStartObject()
+            .writeKey("message2")
+            .writeString("Mul3 Rul3z")
+            .writeEndObject()
+            .writeEndDocument();
+
+    wsw.mergeStream(wsw2.getResult());
+    wsw.writeString("TEST STRING");
+
+    wsw.writeEndDocument();
+
+    WeaveDOMReader wdr = dwbService.getFactory().createDOMReader(wsw.getResult());
+    String msg = (String) wdr.read().evaluateAsObject().get("message").evaluate();
+
+    System.out.println(msg);
+  }
 }
